a65720cd7fe7c03e5f96f8700128c9c3291da288
Andre Adriano <andre@ciclic.com.br>
2021-01-19 21:51:20 -0300

    :sparkles: Create sidebar package

---
+++ packages/sidebar/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sidebar-app">
+    <sidebar />
+  </div>
+</template>
+
+<script>
+import "tailwindcss/tailwind.css";
+import Sidebar from "./components/Sidebar";
+
+export default {
+  name: "SidebarApp",
+  components: {
+    Sidebar,
+  },
+};
+</script>
---
+++ packages/sidebar/src/components/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sidebar">
+    <h1 class="sidebar__title">mfe-lerna-dashboard</h1>
+    <ul class="sidebar__menu">
+      <li class="sidebar__item"><a href="/dashboard">Dashboard</a></li>
+      <li class="sidebar__item"><a href="/options">Options</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.sidebar {
+  @apply h-screen shadow-xl;
+}
+
+.sidebar__title {
+  @apply text-2xl font-bold mb-6 text-center px-10 pt-10 text-gray-700;
+}
+
+.sidebar__item {
+  @apply h-12 px-10 flex flex-1 items-center text-gray-700;
+}
+.sidebar__item:hover {
+  @apply text-blue-500 cursor-pointer transition-all;
+}
+.sidebar__item:hover a {
+  @apply transition-all;
+  filter: drop-shadow(0px 0px 1px rgba(59, 130, 246, 0.5));
+}
+</style>
